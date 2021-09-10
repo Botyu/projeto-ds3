cf5835e6abb03ed637fa7d935353b626cbdfca13
Leonardo Marcondes <leonardomarcondes2002@hotmail.com>
2021-09-10 20:55:42 -0300

    Projeto criado

---
+++ backend/src/test/java/com/vxdev/vxanalyzing/VxanalyzingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vxdev.vxanalyzing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VxanalyzingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
